6693b655df11d6364fe6266d3e33f2db774e82cf
Agbemuko David Ifeoluwa <davidagbemuko.ad@gmail.com>
2022-04-02 10:05:14 +0100

    lab1Assignment

---
+++ Lab_1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+string name = "David Agbemuko";
+string track = "Backend Development Track";
+string learningReason = "personal development and growth";
+ 
+
+Console.WriteLine($"Hello {name}, I am in {track} of Wella Schools.");
+Console.WriteLine($"I chose the track for {learningReason}");
